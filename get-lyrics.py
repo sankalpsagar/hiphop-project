cb909590ca281934c936f63b3737068cbeed3b73
Sankalp Sagar <48313574+sankalpsagar@users.noreply.github.com>
2020-12-26 22:43:29 +0530

    Delete get-lyrics.py

--- get-lyrics.py
+++
@@ -1,9 +0,0 @@
-import lyricsgenius
-genius = lyricsgenius.Genius("9Ut8vL84pWhIkO1CBGuCI9CHdmdrGuQjlYISb4QhNwVLrSlTPc5OFSDReU0I6e8T")
-
-def getLyrics(Song = "Roxanne", Artist= "Arizona Zervas"):
-	# artist = genius.search_artist(Artist, max_songs = 1)
-	song = genius.search_song(Song, Artist)
-	print(song.lyrics)
-
-getLyrics()
